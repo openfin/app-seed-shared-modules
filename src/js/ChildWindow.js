7ee7258ef7459b0b9de54bd02ff896efd28f3182
mrClapham <info@gcdesign.co.uk>
2016-07-15 14:34:17 +0100

    Added the header to the child window.

--- src/js/ChildWindow.js
+++ src/js/ChildWindow.js
@@ -1,8 +1,18 @@
 /**
  * Created by grahamclapham on 14/07/16.
  */
+
+import 'core-js';
+import HeaderVanilla from '../js/vanilla_conmponents/vanilla-header.js';
+// The compiled sass files
+require  ("../sass/entry.scss");
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
-   alert("Here is the child window - loaded");
+   console.log("Here is the child window - loaded");
+
+   let _header = HeaderVanilla.create({text:"This is the new text..."});
+   _header.render(document.querySelector('#vanilla-header'));
 });
 
 
